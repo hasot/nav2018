f87d72e6ef567f63fcc4cb2323f02ad1ffc2f7a2
torte478 <torte478@gmail.com>
2018-01-14 22:34:16 +0300

    add heavy enemy animation

--- js/heavyEnemy.js
+++ js/heavyEnemy.js
@@ -9,24 +9,36 @@ HeavyEnemy = function(x, y, minX, maxX, useHelmet, useShield, game)
 	this.isAlive = true;
 	this.isHeavy = true;
 
+	this.sprite.animations.add('runR', [2, 3], 3, true);
+    this.sprite.animations.add('runL', [4, 5], 3, true);
+    this.sprite.animations.play('runL');
+
 	this.useHelmet = useHelmet;
 	if (useHelmet)
 	{
 		this.helmet = game.add.sprite(x, y, 'heavyEnemyHelmet');
+		this.helmet.frame = 1;
 	}
 	this.useShield = useShield;
 	if (useShield)
 	{
 		this.shield = game.add.sprite(x, y - 16, 'heavyEnemyShield');
+		this.shield.frame = 1;
 	}
 }
 
 HeavyEnemy.prototype.update = function()
 {
     if (this.sprite.body.x < this.minX)
+    {
+    	this.sprite.animations.play('runR');
         this.direction = 1;
+    }
     else if (this.sprite.body.x > this.maxX)
+    {
+    	this.sprite.animations.play('runL');
         this.direction = -1;
+    }
 
     this.sprite.body.velocity.x = EnemySpeed * this.direction;
 
@@ -34,11 +46,13 @@ HeavyEnemy.prototype.update = function()
     {
     	this.helmet.x = this.sprite.x;
     	this.helmet.y = this.sprite.y;
+    	this.helmet.frame = this.direction == 1 ? 0 : 1;
     }
     if (this.useShield)
     {
-    	this.shield.x = this.sprite.x  + (this.direction * 16);
+    	this.shield.x = this.sprite.x  + (this.direction * 8);
     	this.shield.y = this.sprite.y;
+    	this.shield.frame = this.direction == 1 ? 0 : 1;
     }
 };
 
