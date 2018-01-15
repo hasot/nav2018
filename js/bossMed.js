c14fa56b3e961e05ba3e5283aca492fe2cd30dce
torte478 <torte478@gmail.com>
2018-01-15 18:33:07 +0300

    add base boss logic

---
+++ js/bossMed.js
@@ -0,0 +1,117 @@
+BossMed = function(x, y)
+{
+	this.sprite = game.add.sprite(x, y, 'bossMed');
+
+	this.groundPos = 401;
+	this.landingDist = 140;
+
+	this.downSpeed = 4;
+	this.horSpeed = 1;
+	this.upSpeed = 8;
+
+	this.shotPositions = [
+		new Phaser.Point(384, 210),
+		new Phaser.Point(277, 315),
+		new Phaser.Point(553, 371),
+		];
+
+	this.timer = 0;
+	this.state = 'stand';
+	this.subState = 'stand';
+	this.targetPos = new Phaser.Point(0, 0);
+	this.prevPosIndex = -1;
+}
+
+BossMed.prototype.update = function() 
+{
+	if (this.state == 'stand')
+		this.standState();
+	else if (this.state == 'walk')
+		this.walkState();
+};
+
+BossMed.prototype.standState = function() 
+{
+	var waitingEnds = this.timer <= 0;
+	if (waitingEnds)
+		this.startChangePosition();	
+	else
+		this.timer -= 1;
+};
+
+BossMed.prototype.startChangePosition = function() 
+{
+	var index = getRandomInt(0, this.shotPositions.length);
+	if (index == this.prevPosIndex)
+		index = (index + 1) % this.shotPositions.length;
+	
+	this.targetPos = this.shotPositions[index];
+	this.prevPosIndex = index;
+
+	this.state = 'walk';
+	this.subState = 'down';
+};
+
+BossMed.prototype.walkState = function()
+{
+	if (this.subState == 'walk')
+		this.walkSubState();
+	else if (this.subState == 'down')
+		this.downSubState();
+	else if (this.subState == 'up')
+		this.upSubState();
+	else if (this.subState == 'landing')
+		this.landingSubState();
+};
+
+BossMed.prototype.walkSubState = function()
+{
+	var onXTargetPos = this.sprite.x == this.targetPos.x;
+	if (onXTargetPos)
+	{
+		this.subState = 'up';
+	}
+	else
+	{
+		var direction = Math.sign(this.targetPos.x - this.sprite.x);
+		this.sprite.x += direction * this.horSpeed;
+	}
+}
+
+BossMed.prototype.downSubState = function()
+{
+	var onGround = Math.abs(this.sprite.y - this.groundPos) <= this.downSpeed;
+	if (onGround)
+	{
+		this.sprite.y = this.groundPos;
+		this.subState = 'walk';
+	}
+	else
+	{
+		this.sprite.y += this.downSpeed;
+	}
+}
+
+BossMed.prototype.upSubState = function()
+{
+	var needLanding = this.targetPos.y - this.sprite.y > this.landingDist;
+	if (needLanding)
+		this.subState = 'landing';
+	else
+		this.sprite.y -= this.upSpeed;
+}
+
+BossMed.prototype.landingSubState = function()
+{
+	var targetDist = Math.abs(this.sprite.y - this.targetPos.y);
+	var onTargetPos = targetDist <= this.downSpeed;
+	if (onTargetPos)
+	{
+		this.state = 'stand';
+		this.timer = 50;
+	}
+	else
+	{
+		this.sprite.y += this.downSpeed;
+	}
+}
