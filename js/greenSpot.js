043a859550eb8b85a02f14d943bf4c7a0ea465a7
torte478 <torte478@gmail.com>
2018-01-27 09:57:44 +0300

    fix green spot alpha

--- js/greenSpot.js
+++ js/greenSpot.js
@@ -6,10 +6,12 @@ GreenSpot = function()
 GreenSpot.prototype.update = function() 
 {
 	this.sprite.bringToTop();
+	this.sprite.x = game.camera.view.x;
+	this.sprite.y = game.camera.view.y;
 
 	if (this.sprite.alpha > 0)
 	{
-		this.sprite.alpha -= 0.005;
+		this.sprite.alpha = Math.max(0, this.sprite.alpha - 0.005);
 	}
 	else
 	{
